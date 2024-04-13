607e4b14b22c76d7edf68e92bb9f9d550c4ec703
Gabriel <gabriel.ache7@gmail.com>
2024-04-13 02:04:52 -0300

    endpoint integrantes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
 
+let data = {
+    "integrantes":[
+        {'nome': 'Gabriel Ache de Souza'},
+        {'nome': 'Matheus Marques dos Reis'},
+        {'nome': 'Victor Piva'}
+    ]
+};
+
 app.get('/',(req, res)=>{
     res.sendFile('views/index.html', {root:__dirname})
 })
+
+app.get('/integrantes', (req, res) => {
+    res.json(data)
+    })
+
+app.listen(3000,()=> {
+    console.log('listening on : 3000');
+});
